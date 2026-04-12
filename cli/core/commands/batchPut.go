e6deca145bd38457602936efbbe9bf1e047e094f
AatirNadim <aatir.nadim@gmail.com>
2026-04-13 03:13:51 +0530

    feat: update batchDelete and batchPut commands to accept JSON data via --data flag

--- cli/core/commands/batchPut.go
+++ cli/core/commands/batchPut.go
@@ -14,27 +14,10 @@ import (
 
 var BatchPutCmd = &cobra.Command{
 	Use:   "batchPut [jsonFilePath]",
-	Short: "Batch put key-value pairs from a JSON file into the store",
-	Args:  cobra.ExactArgs(1),
+	Short: "Batch put key-value pairs from either a JSON file or via --data flag into the store",
+	Args:  cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-
-		jsonFilePath := args[0]
-
-		err := utils.ValidateJSONAndFilePath(jsonFilePath)
-		if err != nil {
-			return fmt.Errorf("failed to validate JSON file '%s': %w", jsonFilePath, err)
-		}
-
-		fileContent, err := os.ReadFile(jsonFilePath)
-		if err != nil {
-			return fmt.Errorf("failed to read file '%s': %w", jsonFilePath, err)
-		}
-
-		// being able to parse the JSON input file in the desired format is important!
-		var keyValuePairs map[string]string
-		if err := json.Unmarshal(fileContent, &keyValuePairs); err != nil {
-			return fmt.Errorf("failed to parse JSON file '%s': %w", jsonFilePath, err)
-		}
+		dataFlag, _ := cmd.Flags().GetString("data")
 
 		serviceLayer, ok := cmd.Context().Value("serviceLayer").(*service.ServiceLayer)
 
@@ -42,17 +25,53 @@ var BatchPutCmd = &cobra.Command{
 			return fmt.Errorf("service layer not found in context")
 		}
 
-		err = serviceLayer.BatchPutService(keyValuePairs)
+		var keyValuePairs map[string]string
+		var sourceName string
 
-		logger.Debug("Performed BATCH PUT request with data from file:", jsonFilePath)
+		if dataFlag != "" {
+			if !json.Valid([]byte(dataFlag)) {
+				return fmt.Errorf("provided data flag is not valid JSON")
+			}
+			if err := json.Unmarshal([]byte(dataFlag), &keyValuePairs); err != nil {
+				return fmt.Errorf("failed to parse JSON data: %w", err)
+			}
+			sourceName = "provided data flag"
+		} else {
+			if len(args) == 0 {
+				return fmt.Errorf("must provide either a JSON file path as a positional argument or the --data flag")
+			}
+			jsonFilePath := args[0]
+			err := utils.ValidateJSONAndFilePath(jsonFilePath)
+			if err != nil {
+				return fmt.Errorf("failed to validate JSON file '%s': %w", jsonFilePath, err)
+			}
+
+			fileContent, err := os.ReadFile(jsonFilePath)
+			if err != nil {
+				return fmt.Errorf("failed to read file '%s': %w", jsonFilePath, err)
+			}
+
+			if err := json.Unmarshal(fileContent, &keyValuePairs); err != nil {
+				return fmt.Errorf("failed to parse JSON file '%s': %w", jsonFilePath, err)
+			}
+			sourceName = fmt.Sprintf("file '%s'", jsonFilePath)
+		}
+
+		err := serviceLayer.BatchPutService(keyValuePairs)
+
+		logger.Debug("Performed BATCH PUT request with data from:", sourceName)
 
 		if err != nil {
 			logger.Error("Error occurred while making BATCH PUT request:", err)
-			return fmt.Errorf("failed to perform batch put with data from file '%s': %w", jsonFilePath, err)
+			return fmt.Errorf("failed to perform batch put with data from %s: %w", sourceName, err)
 		}
 
-		fmt.Println("Batch put operation successful for key-value pairs from file:", jsonFilePath)
+		fmt.Println("Batch put operation successful for key-value pairs from:", sourceName)
 
 		return nil
 	},
+}
+
+func init() {
+	BatchPutCmd.Flags().StringP("data", "d", "", "JSON data payload")
 }
